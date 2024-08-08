6fc6dd192e3bc5a15ce0426ed430a99de9a0e279
Gustavo Paredes <bryangustavoparedes@gmail.com>
2024-08-08 11:39:04 -0500

    Setup complet folder structure

---
+++ template.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from pathlib import Path
+import logging
+
+# List of directories to create
+
+while True:
+    project_name = input("Enter the project name: ")
+    if project_name != "":
+        break
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/config/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/utils/__init__.py",
+    "config/config.yaml",
+    "schema.yaml",
+    "app.py",
+    "main.py",
+    "logs.py",
+    "exception.py",
+    "setup.py",
+]
+
+# Create directories
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+    
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+    
+    else:
+        logging.info(f"File already exists: {file_path}")
+
+    
